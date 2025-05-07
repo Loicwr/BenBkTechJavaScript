f8c313cc70ba3d99257a366106ca48b1fe9f46e9
Loicwr <nephtyse19@hotmail.fr>
2025-05-07 15:27:01 +0200

    test

---
+++ formationjavascript/variablesEtConstante.js
@@ -0,0 +1,88 @@
+
+/*
+
+Test de variable avec : let
+
+let monNombre = 2;
+monNombre = 888;
+console.log(monNombre);
+let maPhrase = "Bonjour !";
+let maPhrase2 = 'Bonjour2 !';
+let maPhrase3 = `Bonjour3 !`;
+console.log(maPhrase);
+console.log(maPhrase2);
+console.log(maPhrase3);
+
+*/ 
+
+
+/*
+
+Test de variable avec : const ( avec const, la valeur est constante, elle ne peut pas être changée )
+
+const monNombre = 2;
+console.log(monNombre);
+monNombre = 5;
+console.log(monNombre);
+
+
+*/ 
+
+/* Test avec typeof 
+
+// Types de variable  ( dynamique )
+
+let monNombre = 2; // number (nombre)
+let monNombre2 = -2;
+let monNombre3 = 0.3;
+let maPhrase = "Ma phrase"; // string ( chaîne de caractères )
+let monBoolen = true; //boolean ( true ou false )
+
+//typeof : typeof renvoie le type d'une certaine variable
+
+console.log (typeof monNombre);
+console.log (typeof monNombre2);
+console.log (typeof monNombre3);
+console.log (typeof maPhrase);
+console.log (typeof monBoolen);
+
+*/
+
+/*
+// Les opérateurs arithmétiques ( + = addition, - = soustraction, * = multiplication, / division, % = modulo :(reste d'une division)) 
+// Façon classique :
+let resultat = 5 - 3;
+resultat = resultat * 3;
+console.log(resultat);
+
+// Façon simplifiée : (+= , -=, *=, /=, %=)
+let resultat2 = 5 - 3;
+resultat2 *= 3; // resultat = resultat * 3
+resultat2 += 2; // resultat = resultat + 2
+console.log(resultat2);
+
+// Exercice 
+
+let exercice = 5 * 5; // peut être aussi écrit : let exercice = ( 5 * 5 ) + 2 
+exercice += 2;
+console.log (exercice);
+
+let exercice2 = 60 / 4; // peut être aussi écrit : let exercice2 = ( 60 / 4 ) - 6
+exercice2 -= 6;
+console.log (exercice2);
+
+let exercice3 = 5555 * 7777;
+console.log (exercice3);
+
+let exercice4 = 938594 - 94859304;// peut être aussi écrit : let exercice4 = ( 938594 - 94859304 ) * 948
+exercice4 *= 948;
+console.log (exercice4);
+
+let exercice5 = 4 * 4 * 6; // peut être aussi écrit : let exercice5 = ( 4 * 4 * 6 ) / 5
+exercice5 /= 5;            // exercices5 *= 99
+exercice5 *= 99;
+console.log (exercice5);
+
+*/
+
+// La concaténation est l'opération qui consiste à assembler des chaînes de caractères entre elles.
