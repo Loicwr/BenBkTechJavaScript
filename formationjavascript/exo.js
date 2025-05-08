21af83d575447b382fc6d02f55bf8b822fd8b201
Loicwr <nephtyse19@hotmail.fr>
2025-05-08 14:01:58 +0200

    Add initial implementation of bank account and age validation logic

---
+++ formationjavascript/exo.js
@@ -0,0 +1,16 @@
+let compteEnBanque = 859;
+let age = 19;
+let objet = 859;
+
+if ( compteEnBanque >= objet && age >= 18 )
+    {
+    console.log ( "Vous pouvez acheter l'objet")
+}
+else  {
+    console.log ( " Vous ne pouvez pas acheter l'objet")
+}
+
+
+let valeurCarre = 18;
+valeurCarre *= valeurCarre;
+console.log (valeurCarre);
