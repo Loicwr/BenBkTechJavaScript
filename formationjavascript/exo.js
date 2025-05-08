6cb77b4d4a44513968be46d298f7ce42985b5507
Loicwr <nephtyse19@hotmail.fr>
2025-05-08 14:33:54 +0200

    Fix perimeter calculation for circle and add average grade evaluation logic

--- formationjavascript/exo.js
+++ formationjavascript/exo.js
@@ -15,8 +15,8 @@ let valeurCarre = 18;
 valeurCarre *= valeurCarre;
 console.log (valeurCarre);
 
-let rayonCercle = 18
-perimetre = 2 * 3.14 * rayonCercle
+let rayonCercle = 1
+let perimetre = 2 * 3.14 * rayonCercle
 if ( perimetre > 100)
     {
         console.log ("Le cercle a une périphérie très longue")
@@ -25,3 +25,20 @@ else {
         console.log (perimetre)
 }
 
+let noteFrancais = 10;
+let noteMath = 11;
+let noteSvt =  16;
+
+let moyennes = ( noteFrancais + noteMath + noteSvt ) / 3;
+if ( moyennes >= 0 && moyennes <= 4.99){
+    console.log ( "Insuffisant" )
+}
+else if ( moyennes >= 5 && moyennes <= 9.99 ){
+    console.log ("Bof bof")
+}
+else if (moyennes >= 10 && moyennes <= 14.99 ){
+    console.log ("Bien !")
+}
+else if ( moyennes >= 15 && moyennes <= 20 ){
+    console.log ("Très bien !")
+}
